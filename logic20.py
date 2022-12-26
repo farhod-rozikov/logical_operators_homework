09f986daa4e25277aebfd60cb1df2e7db51fba97
Farhod Rozikov <fara.rozikov@gmail.com>
2022-12-26 22:46:35 +0500

    logic task 20 test

--- logic20.py
+++ logic20.py
@@ -8,5 +8,12 @@ def main(n):
     Returns:
         bool: answer
     """
+    '''d_1 = n % 10
+    d_2 = n // 10 % 10
+    d_3 = n // 100 % 10
+    d_4 = n // 1000 % 10
+    d_5 = n // 10000'''
+    answer = n
+    return answer
 
-    return
+print(main(1100))
