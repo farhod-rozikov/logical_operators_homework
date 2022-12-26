5457269c089ff6a13980cbffc34ec6beade74291
Farhod Rozikov <fara.rozikov@gmail.com>
2022-12-26 21:40:36 +0500

    logic task-18

--- logic18.py
+++ logic18.py
@@ -5,4 +5,12 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    d_1 = a % 10
+    d_2 = a // 10 % 10
+    d_3 = a // 100 % 10
+    d_4 = a // 1000 % 10
+    d_5 = a // 10000
+    answer = d_5 > d_4 and d_4 > d_3 and d_3 > d_2 and d_2 > d_1
+    return answer
+
+print(main(54321))
