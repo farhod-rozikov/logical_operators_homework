813077a865399074228100979c2d0c88455e3fce
Farhod Rozikov <fara.rozikov@gmail.com>
2022-12-26 21:35:14 +0500

    logic task 10

--- logic10.py
+++ logic10.py
@@ -1,3 +1,6 @@
+from numpy import ma
+
+
 def main(a):
     """
     Given integer a,  check the following statement "The integer is two-digit number".
@@ -6,4 +9,7 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    answer = a > 9 and a < 100
+    return answer
+
+print(main(10))
