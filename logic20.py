276af50341d51430fc35d0bff2a800d6093b21ac
Farhod Rozikov <fara.rozikov@gmail.com>
2022-12-27 18:56:16 +0500

    logic task 20'

--- logic20.py
+++ logic20.py
@@ -8,18 +8,24 @@ def main(n):
     Returns:
         bool: answer
     """
-    d_1 = n % 10 == 1
-    d_1 += n // 10 % 10 == 1
-    d_1 += n // 100 % 10 == 1
-    d_1 += n // 1000 % 10 == 1
-    d_1 += n // 10000
+    d_1 = n % 10
+    d_2 = n // 10 % 10
+    d_3 = n // 100 % 10
+    d_4 = n // 1000 % 10
+    d_5 = n // 10000
+ 
+    one_count = d_1 == 1
+    one_count += d_2 == 1
+    one_count += d_3 == 1
+    one_count += d_4 == 1
+    one_count += d_5 == 1
 
-    d_0 = n % 10 == 0
-    d_0 += n // 10 % 10 == 0
-    d_0 += n // 100 % 10 == 0
-    d_0 += n // 1000 % 10 == 0
+    zero_count = d_1 == 0 and (d_2 or d_3 or d_4 or d_5)
+    zero_count += d_2 == 0 and (d_3 or d_4 or d_5)
+    zero_count += d_3 == 0 and (d_4 or d_5)
+    zero_count += d_4 == 0 and d_5
     
-    answer = d_1 > d_0
+    answer = one_count > zero_count
     return answer
 
-print(main(100))
+print(main(11100))
